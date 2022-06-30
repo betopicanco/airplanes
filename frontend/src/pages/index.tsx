f6a437ac11ff5e803feb0de3433f5032ac6e534a
betopicanco <betopicanco@gmail.com>
2022-06-30 11:04:47 -0300

    start project

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className={` text-3xl font-bold underline `}>
+      <p>Hello World!</p>
+    </div>
+  )
+}
+
+export default Home
